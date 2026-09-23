fdd0d4396b83029215dd81f1ddf8c94d6112f3e3
jeonga071 <na5730456@gmail.com>
2026-09-23 21:48:41 +0900

    health.txt 파일 읽고 리스트 생성

---
+++ first/BMI.py
@@ -0,0 +1,9 @@
+with open("health.txt", "r", encoding="utf-8") as file: #파일 읽기. 인코딩 필요.
+    file.readline() # 헤더 부분만 읽음
+
+    data = []
+
+    for line in file:
+        data.append(line.strip())
+
+print(data)
